321bf643b28f39df0c13a6b828fe1edb7f41aa60
SebasXayala <juansebastianayala48@gmail.com>
2023-04-20 13:45:11 -0500

    se creo la clase prevestir que hereda de la clase Producto con los atributos talla, marca y color

---
+++ src/practico1/prevestir.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practico1;
+
+/**
+ *
+ * @author 416-pc6
+ */
+public class prevestir {
+    
+}
